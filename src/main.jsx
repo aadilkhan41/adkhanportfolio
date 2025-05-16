362ddd69dc8fa99a2214b30e9a959d4837285854
Aadil <aadilkhan3007@gmail.com>
2025-05-16 15:50:40 +0530

    Added Vanta Bg

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home.jsx'
+import VantaBodyBackground from './components/VantaBodyBackground/VantaBodyBackground.jsx';
 
 createRoot(document.getElementById('root')).render(
-    <Home />,
+    <>
+        <VantaBodyBackground />
+        <div style={{ position: "relative", zIndex: 1 }}>
+            <Home />
+        </div>
+    </>,
 );
